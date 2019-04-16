d8c2709d0fa2a1d14a6662fce988c12c627a293f
sabinbogati1 <sabin.bogati@studentpartner.com>
2019-04-16 09:15:26 +0545

    More profile API..

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -37,6 +37,70 @@ router.get("/", passport.authenticate("jwt", {session: false}), (req,res) =>{
 });
 
 
+//@route GET api/profile/all
+//@desc Get all profile
+//@access PUBLIC
+
+router.get("/all", (req,res) =>{
+    const errors = {};
+    Profile.find()
+        .populate("user", ["name", "avatar"])
+        .then(profile =>{
+            if(!profile){
+                errors.noprofile = "There is no profiles";
+                return res.status(404).json(errors);
+            }
+            res.json(profile);
+        })
+        .catch(err => res.status(404).json(
+            {profile: "There is no profiles"}
+        ));
+})
+
+
+
+//@route GET api/profile/handle/:handle
+//@desc Get profile by handle
+//@access PUBLIC
+router.get("/handle/:handle", (req,res) =>{
+    const errors = {};
+    Profile.findOne({handle: req.params.handle})
+            .populate("user", ["name", "avatar"])
+            .then(profile =>{
+                if(!profile){
+                    errors.noprofile = "There is no profile for this user";
+                    res.status(404).json(errors);
+                }
+                    res.json(profile);
+            })
+            .catch(err => res.status(404).json(err));
+
+})
+
+
+//@route GET api/profile/user/:user_id
+//@desc Get profile by user ID
+//@access PUBLIC
+router.get("/user/:user_id", (req,res) =>{ 
+    const errors = {};
+    Profile.findOne({user: req.params.user_id})
+            .populate("user", ["name", "avatar"])
+            .then(profile =>{
+                if(!profile){
+                    errors.noprofile = "There is no profile for this user";
+                    res.status(404).json(errors);
+                }
+                    res.json(profile);
+            })
+            .catch(err => res.status(404).json(
+                {profile: "There is no profile for this user"}
+            ));
+
+})
+
+
+
+
 //@route POST api/profile
 //@desc CREATE or UPDATE User profile
 //@access PRIVATE
