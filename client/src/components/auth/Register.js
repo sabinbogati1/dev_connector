f83ed42ba36a881538582dab8cea252c33a5d4a4
sabinbogati1 <sabin.bogati@studentpartner.com>
2019-04-18 16:22:12 +0545

    Creating the register form with state..

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,10 +1,70 @@
 import React, { Component } from 'react'
 
- class Register extends Component {
+class Register extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            name: "",
+            email: "",
+            password: "",
+            password2: "",
+            errors: {}
+        }
+
+        this.onchange = this.onchange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+        const { name, email, password, password2 } = this.state;
+        const newUser = {
+            name,
+            email,
+            password,
+            password2
+        }
+        console.log("newUser :: ", newUser);
+    }
+
+    onchange(e) {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
   render() {
     return (
-      <div>
-          Register
+        <div className="register">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 m-auto">
+              <h1 className="display-4 text-center">Sign Up</h1>
+              <p className="lead text-center">Create your DevConnector account</p>
+              <form onSubmit={this.onSubmit}>
+                <div className="form-group">
+                                <input type="text" className="form-control form-control-lg"
+                                    placeholder="Name" name="name" value={this.state.name} onChange={this.onchange} />
+                </div>
+                <div className="form-group">
+                                <input type="email" className="form-control form-control-lg"
+                                    placeholder="Email Address" name="email" value={this.state.email} onChange={this.onchange} />
+                  <small className="form-text text-muted">This site uses Gravatar so if you want a profile image, use a Gravatar email</small>
+                </div>
+                <div className="form-group">
+                                <input type="password" className="form-control form-control-lg"
+                                    placeholder="Password" name="password" value={this.state.password} onChange={this.onchange} />
+                </div>
+                <div className="form-group">
+                                <input type="password" className="form-control form-control-lg"
+                                    placeholder="Confirm Password" name="password2" value={this.state.password2} onChange={this.onchange}/>
+                </div>
+                <input type="submit" className="btn btn-info btn-block mt-4" />
+              </form>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
