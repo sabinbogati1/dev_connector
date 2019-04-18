69ff5151dfc8dd442e7c2a774be7c29966ec34d0
sabinbogati1 <sabin.bogati@studentpartner.com>
2019-04-18 18:05:51 +0545

    Added redux..

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Provider } from "react-redux";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import Landing from "./components/layout/Landing";
@@ -10,17 +11,19 @@ import "./App.css";
 class App extends Component {
 	render() {
 		return (
-			<Router>
-				<div className="App">
-					<Navbar />
-          <Route exact path="/" component={Landing} />
-          <div className="container">
-            <Route exact path="/register" component={Register} />
-            <Route exact path = "/login" component={Login} />
-          </div>
-					<Footer />
-				</div>
-			</Router>
+			<Provider store={store}>
+				<Router>
+					<div className="App">
+						<Navbar />
+						<Route exact path="/" component={Landing} />
+						<div className="container">
+							<Route exact path="/register" component={Register} />
+							<Route exact path="/login" component={Login} />
+						</div>
+						<Footer />
+					</div>
+				</Router>
+			</Provider>
 		);
 	}
 }
