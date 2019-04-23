190062bd7ad813553e90a26b877cca2e4f2d061d
sabinbogati1 <sabin.bogati@studentpartner.com>
2019-04-23 09:33:13 +0545

    Added create profile action..

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -17,6 +17,22 @@ export const getCurrentProfile = () => dispatch => {
     })
 }
 
+//Create Profile
+export const createProfile = (profileData, history) => dispatch =>{
+
+    axios.post("/api/profile", profile).then(
+        res => {
+            history.push("/dashboard")
+        }
+    ).catch(err =>{
+        dispatch({
+            type: GET_ERRORS,
+            payload: err.response.data
+        })
+    })
+
+}
+
 //Profile Loading
 export const setProfileLoading = () => {
     return {
--- client/src/components/create-profile/CreateProfile.js
+++ client/src/components/create-profile/CreateProfile.js
@@ -30,6 +30,14 @@ class CreateProfile extends Component {
 		this.onSubmit = this.onSubmit.bind(this);
   }
 
+  componentWillReceiveProps(nextProps){
+      if(nextProps.errors){
+        this.setState({
+          errors: nextProps.errors
+        })
+      }
+  }
+
 
   onSubmit(e) {
     e.preventDefault();
